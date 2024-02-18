b87dfba8529a50210ecdfa2f131ec5b6880b2b30
xyc <>
2024-02-18 15:51:55 +0800

    update: 图标、app名称、启动图替换

--- lib/pages/main/views/splash.dart
+++ lib/pages/main/views/splash.dart
@@ -23,27 +23,31 @@ class _SplashState extends State<Splash> {
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+    );
     return SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(height: 271.h),
+          SizedBox(height: 168.h),
           Center(
             child: Image.asset('assets/main/main_launch_logo.png',
-                width: 237.w, height: 166.w),
+                width: 142.w, height: 134.w),
           ),
-          SizedBox(height: 5.h),
+          SizedBox(height: 22.h),
           Center(
-            child: Text("Lend Funds",
+            child: Text("Sahayak Cash",
                 style: TextStyle(
-                    fontSize: 36.sp,
-                    color: const Color(0xFF003C6A),
-                    fontWeight: FontWeight.w500)),
+                    fontSize: 30.sp,
+                    color: const Color(0xFF000000),
+                    fontWeight: FontWeight.w700)),
           ),
+          SizedBox(height: 30.h),
           Center(
-            child: Text("Your personal ATM",
+            child: Text("A lending app used by more than 10,000 people",
                 style: TextStyle(
-                    fontSize: 20.sp,
-                    color: const Color(0xFF003C6A),
+                    fontSize: 12.5.sp,
+                    color: const Color(0xFF000000),
                     fontWeight: FontWeight.w500)),
           ),
         ],
