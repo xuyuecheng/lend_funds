9802e97df0f2ca14d4c7288590c93100b73839f5
coder <>
2024-03-13 18:03:51 +0800

    update: branch接入完成（后续要有开发者账号，Associated Domains后续要在开发者账号那边开启，id申请完后要替换id）

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
 #import "CZDeviceUtils.h"
+#import <Branch.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
@@ -8,7 +9,29 @@
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
     [[CZDeviceUtils sharedManager] getDeviceInfo];
+    // Listener for Branch deep link data
+     [[Branch getInstance] initSessionWithLaunchOptions:launchOptions andRegisterDeepLinkHandler:^(NSDictionary * _Nonnull params, NSError * _Nullable error) {
+         NSLog(@"11112222%@,userInfo:%@,code:%ld,domain:%@", params,error.userInfo,(long)error.code,error.domain);
+           // Access and use deep link data here (nav to page, display content, etc.)
+     }];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  [[Branch getInstance] application:app openURL:url options:options];
+  return YES;
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler {
+  // Handler for Universal Links
+  [[Branch getInstance] continueUserActivity:userActivity];
+  return YES;
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+  // Handler for Push Notifications
+  [[Branch getInstance] handlePushNotification:userInfo];
 }
 
 @end
